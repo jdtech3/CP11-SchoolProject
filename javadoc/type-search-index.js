b0180f7f74acfc1614661a13f7c94ef6e2a154b3
Joe Dai <dm.joedai@gmail.com>
2019-08-28 13:35:27 +0800

    Generate javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"xyz.joedai.schoolproj","l":"Main"},{"p":"xyz.joedai.schoolproj","l":"School"},{"p":"xyz.joedai.schoolproj","l":"Student"},{"p":"xyz.joedai.schoolproj","l":"Teacher"}]
